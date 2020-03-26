b5c1ed3457d5adf12c20a1848646153778aefbf1
David Perez <david@closemarketing.es>
2020-03-26 17:11:33 +0100

    options

--- includes/class-options.php
+++ includes/class-options.php
@@ -34,18 +34,18 @@ class NP_Admin {
 
 		add_submenu_page(
 			'options-general.php',
-			__( 'Software Connections', 'noindex-seo' ),
-			__( 'Connections', 'noindex-seo' ),
+			__( 'No Index Settings', 'noindex-seo' ),
+			__( 'NoIndex', 'noindex-seo' ),
 			'manage_options',
 			'noindex_pages_settings',
 			array(
 				$this,
-				'erp_plugin_settings_page'
+				'plugin_settings_page',
 			),
 		);
 
 		// Call register settings function.
-		add_action( 'admin_init', array( $this, 'register_erp_settings' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
 	/**
@@ -53,7 +53,7 @@ class NP_Admin {
 	 *
 	 * @return void
 	 */
-	public function register_erp_settings() {
+	public function register_settings() {
 		// Register our settings.
 		register_setting( 'noindex_pages_settings', 'botcamp_erp' );
 		register_setting( 'noindex_pages_settings', 'botcamp_erp_apipass' );
@@ -61,46 +61,33 @@ class NP_Admin {
 		register_setting( 'noindex_pages_settings', 'botcamp_project_apipass' );
 	}
 
+	private function select_table_html( $variable, $options ) {
+		$variable_value = get_option( $variable );
+		// Language.
+		$html = '<select name="' . $variable . '"><option value=""';
+		if ( ! $variable_value ) {
+			$html .= ' selected';
+		}
+		$html .= '></option>';
+		foreach ( $options as $key => $value ) {
+			$html .= '<option value="' . $key . '"';
+			if ( ( $variable_value === $key ) || ( ! $variable_value && 1 === $key ) ) {
+				$html .= ' selected';
+			}
+			$html .= '>' . $value . '</option>';
+		}
+		$html .= '</select>';
+
+		echo $html;
+	}
+
 	/**
 	 * Options page for ERP Plugin
 	 *
 	 * @return void
 	 */
-	public function erp_plugin_settings_page() {
-		$erp = get_option( 'botcamp_erp' );
-		// Language.
-		$select_erp = '<option value=""';
-		if ( ! $erp ) {
-			$select_erp .= ' selected';
-		}
-		$select_erp   .= '></option>';
-		$erp_selection = array(
-			'holded' => __( 'Holded', 'noindex-seo' ),
-		);
-		foreach ( $erp_selection as $key => $value ) {
-			$select_erp .= '<option value="' . $key . '"';
-			if ( ( $erp === $key ) || ( ! $erp && 1 === $key ) ) {
-				$select_erp .= ' selected';
-			}
-			$select_erp .= '>' . $value . '</option>';
-		}
-		$project = get_option( 'botcamp_project' );
-		// Language.
-		$select_project = '<option value=""';
-		if ( ! $project ) {
-			$select_project .= ' selected';
-		}
-		$select_project   .= '></option>';
-		$project_selection = array(
-			'asana' => __( 'Asana', 'noindex-seo' ),
-		);
-		foreach ( $project_selection as $key => $value ) {
-			$select_project .= '<option value="' . $key . '"';
-			if ( ( $project === $key ) || ( ! $project && 1 === $key ) ) {
-				$select_project .= ' selected';
-			}
-			$select_project .= '>' . $value . '</option>';
-		}
+	public function plugin_settings_page() {
+		$noindex_seo_type = get_option( 'noindex_seo_type' );
 		?>
 		<div class="wrap">
 		<h1><?php esc_html_e( 'Software Connections', 'noindex-seo' ); ?></h1>
@@ -112,26 +99,36 @@ class NP_Admin {
 			<hr/>
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row"><?php esc_html_e( 'Select ERP', 'import-inmovilla-properties' ); ?></th>
-					<td><select name="botcamp_erp"><?php echo $select_erp; ?></select></td>
-				</tr>
-				<tr valign="top">
-					<th scope="row"><?php esc_html_e( 'API Password', 'noindex-seo' ); ?></th>
-					<td><input type="text" name="botcamp_erp_apipass" value="<?php echo esc_attr( get_option( 'botcamp_erp_apipass' ) ); ?>" style="min-width: 300px" /></td>
-				</tr>
-			</table>
-			<h2><?php esc_html_e( 'Projects Selections', 'noindex-seo' ); ?></h2>
-			<hr/>
-			<table class="form-table">
-				<tr valign="top">
-					<th scope="row"><?php esc_html_e( 'Select Project', 'import-inmovilla-properties' ); ?></th>
-					<td><select name="botcamp_project"><?php echo $select_project; ?></select></td>
-				</tr>
-				<tr valign="top">
-					<th scope="row"><?php esc_html_e( 'API Password', 'noindex-seo' ); ?></th>
-					<td><input type="text" name="botcamp_project_apipass" value="<?php echo esc_attr( get_option( 'botcamp_project_apipass' ) ); ?>" style="min-width: 300px" /></td>
+					<th scope="row"><?php esc_html_e( 'Type of NoIndex', 'import-inmovilla-properties' ); ?></th>
+					<td>
+						<?php
+						$this->select_table_html(
+							'noindex_seo_type',
+							array(
+								'total'   => __( 'All options recommended', 'noindex-seo' ),
+								'partial' => __( 'Partial selection', 'noindex-seo' ),
+							)
+						);
+						?>
+					</td>
 				</tr>
 			</table>
+			<?php 
+				if ( 'partial' === $noindex_seo_type ) { ?>
+					<h2><?php esc_html_e( 'Projects Selections', 'noindex-seo' ); ?></h2>
+					<hr/>
+					<table class="form-table">
+						<tr valign="top">
+							<th scope="row"><?php esc_html_e( 'Select Project', 'import-inmovilla-properties' ); ?></th>
+							<td><select name="botcamp_project"><?php echo $select_project; ?></select></td>
+						</tr>
+						<tr valign="top">
+							<th scope="row"><?php esc_html_e( 'API Password', 'noindex-seo' ); ?></th>
+							<td><input type="text" name="botcamp_project_apipass" value="<?php echo esc_attr( get_option( 'botcamp_project_apipass' ) ); ?>" style="min-width: 300px" /></td>
+						</tr>
+					</table>
+					<?php
+				} ?> 
 			<?php submit_button(); ?>
 		</form>
 		</div>
